28e9f9799e1edce09e2e77095fb2ddb4457b3145
Ben Daws <ben@bendaws.net>
2024-09-11 09:10:31 -0500

    Hopefully fixed the segfault error

--- benc.c
+++ benc.c
@@ -15,50 +15,141 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
-
-#include "compile.h"
 
 #define BC_VERSION "1.0.0"
 #define BC_COPYRIGHT "(C) Copyright 2024"
 
-char* parse_flag(int argc, char* argv[]) {
-    char* flags;
+#ifdef WIN32
+#include <io.h>
+#define access _access
+#endif
 
-    switch (argc) {
-        case 1:
-            flags = "00n0";
-            break;
-        case 2:
-            char* flag = argv[2];
-            flags = flag;
-            break;
-        default:
-            flags = "00n1";
-            break;
+#define F_OK 0
+
+char* writeFile(char* fname, char* fileContents) {
+    FILE *fptr;
+    fptr = fopen(fname, "w");
+    fprintf(fptr, fileContents);
+    fclose(fptr);
+}
+
+char* readFile(char* fpath) {
+    FILE *fptr;
+    fptr = fopen(fpath, "r");
+
+    char* fileContents;
+
+    char* file = &fileContents;
+    char* fileTxt;
+
+    while(fgets(fileTxt, 99991, fptr)) {
+        file[sizeof(file)] = fileTxt;
     }
 
-    return flags;
+    fclose(fptr);
+
+    return file;
+}
+
+char* split_characters = " ;,+-*&/(){}#";
+
+bool is_split_character(char character) {
+    bool value = false;
+
+    for (int i = 0; i < sizeof(split_characters); i++) {
+        if (character == split_characters[i]) {
+            value = true;
+        }
+    }
+
+    return value;
+}
+
+// Assuming readFile and is_split_character are defined elsewhere
+
+char* bc_compile(char* fpath) {
+    char* lines = readFile(fpath);
+    if (lines == NULL) {
+        return NULL;
+    }
+
+    size_t linesLength = strlen(lines);
+
+    for (size_t i = 0; i < linesLength; i++) {
+        // per line
+        bool dirty = false;
+        char* line = &lines[i];
+        char currentKeyword[256] = ""; // Assuming max keyword length is 255
+        int keywordPos[256] = {-1}; // Assuming max keyword length is 255
+        size_t keywordLength = 0;
+
+        size_t lineLength = strlen(line);
+
+        for (size_t j = 0; j < lineLength; j++) {
+            // per character
+            char thisChar = line[j];
+
+            if (is_split_character(thisChar)) {
+                currentKeyword[0] = '\0';
+                keywordLength = 0;
+            } else {
+                currentKeyword[keywordLength] = thisChar;
+                currentKeyword[keywordLength + 1] = '\0';
+                keywordPos[keywordLength] = j;
+                keywordLength++;
+            }
+
+            if (strcmp(currentKeyword, "string") == 0) {
+                // string, replace the keyword positions with each character
+                char characters[] = {'c', 'h', 'a', 'r', '*'};
+
+                for (size_t k = 0; k < keywordLength; k++) {
+                    if (k == 4) {
+                        // shifts all characters 1 index ahead to effectively "remove" the extra character.
+                        size_t charsToMove = lineLength - keywordPos[k];
+
+                        for (size_t m = keywordPos[k]; m < lineLength - 1; m++) {
+                            // shift character ahead in the string
+                            line[m] = line[m + 1];
+                        }
+
+                        line[lineLength - 1] = '\0'; // finally, get rid of the last character (a dupe)
+                    } else {
+                        line[keywordPos[k]] = characters[k];
+                    }
+                }
+
+                dirty = true;
+                break; // Exit the inner loop as we have processed the keyword
+            }
+        }
+
+        if (dirty) {
+            // Handle the dirty line if needed
+        }
+    }
+
+    return lines;
 }
 
 int main(int argc, char* argv[]) {
     int exit_code = 0; // set to exit code
-    char* flags = parse_flag(argc, argv);
     bool is_file = false;
 
-    if (flags == "00n0") {
-        printf("bc: no arguments given.\n");
-        exit_code = 1;
-    } else if (flags == "00n1") {
-        printf("bc: too many arguments given.\n");
-        exit_code = 1;
-    } else if (flags == "--version") {
+    if (sizeof(argv) == 1) printf("bc: no arguments given.\n"); return 1;
+    if (sizeof(argv) > 2) printf("bc: too many arguments given.\n"); return 1;
+
+    char* flag = argv[1];
+
+    if (flags == "--version") {
         printf("Ben-C compiler version %s\n", BC_VERSION);
         printf("%s\n", BC_COPYRIGHT);
         printf("This program comes with ABSOLUTELY NO WARRANTY;\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; see https://www.gnu.org/licenses.\n");
         exit_code = 0;
     } else {
-        if (access(flags[0], F_OK) == 0) {
+        if (access(flag, F_OK) == 0) {
             // file exists
             exit_code = 0;
             is_file = true;
